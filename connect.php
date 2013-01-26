e921c64eefd704d9fcb05f34bb2f420c0963c14f
guibayer <guibayer2@gmail.com>
2013-01-26 20:29:20 -0200

    Teste no gui

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn = @mysql_connect("localhost", "root", "") or die ("Nome de usuario ou senha incorretos!");
+$conn = @mysql_connect("localhost", "root", "guilherme") or die ("Nome de usuario ou senha incorretos!");
 $db = @mysql_select_db("band", $conn) or die ("Base de dados inesistente!");
 ?>
